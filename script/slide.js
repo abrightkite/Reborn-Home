fc9104996eff6d6c2bb808290f2ae710aa3b9bd4
abrightkite <phy5397@gmail.com>
2021-11-12 09:41:55 +0900

    style

--- script/slide.js
+++ script/slide.js
@@ -13,39 +13,20 @@ $(function(){
 
     // HTML 요소 생성 및 배치 ???
     $slide.each(function(i){
-        $(this).css({left: 100 * i + '%'});
+        $(this).css({left: 50 * i + '%'});
         indicatorHTML += '<a href="#">' + (i+1) +'</a>'
     })
     $indicator.html(indicatorHTML);
 
-    // 모든 슬라이드를 표시하는 함수 (슬라이드 이동함수)
+    // 모든 슬라이드 표시 (슬라이드 이동함수)
     function goToSlide(index) {
-        $slideGroup.animate({left:-100 * index + '%'}, 500);
+        $slideGroup.animate({left:-50 * index + '%'}, 500);
         curIndex = index;
 
         updateNav();
     }
-    // 슬라이드 상태에 따라 탐색 및 표시를 업데이트하는 함수
+    // 슬라이드 상태에 따라 탐색 및 표시를 업데이트
     function updateNav() {
-        var $navPrev = $nav.find('.prev'),
-            $navnext = $nav.find('.next');
-
-        // 처음 curIndex === 0 이전 버튼 안 보이도록
-        // 마지막 curIndex === 3 다음 버튼 안 보이도록
-
-        // 만약 첫번째 슬라이드라면 prev 탐색을 해제
-        if(curIndex === 0) {
-            $navPrev.addClass('disable');
-        } else {
-            $navPrev.removeClass('disable');
-        }
-        // 만약 마지막 슬라이드라면 next 탐색을 해제
-        if(curIndex === slideCount-1) {
-            $navnext.addClass('disable');
-        } else {
-            $navnext.removeClass('disable');
-        }
-
         // 인디게이터 현재 슬라이드 표시 해제
         $indicator.find('a').removeClass('active').eq(curIndex).addClass('active');
     }
@@ -82,7 +63,7 @@ $(function(){
         timer = setInterval(function(){
             var nextIndex = (curIndex + 1) % slideCount
             goToSlide(nextIndex);
-        }, 3000)
+        }, 2000)
     }
     function stopTimer() {
         clearInterval(timer);
