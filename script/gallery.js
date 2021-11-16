2ff46e900c2b8d489ed7ab13fbfe591c89333a5f
abrightkite <phy5397@gmail.com>
2021-11-17 04:09:51 +0900

    Make grid gallery

---
+++ script/gallery.js
@@ -0,0 +1,20 @@
+$(function(){
+
+    $('.grid').imagesLoaded( function() {
+        // images have loaded
+        $('.grid').masonry({
+            // set itemSelector so .grid-sizer is not used in layout
+            itemSelector: '.grid-item',
+            // use element for option
+            columnWidth: '.grid-sizer',
+            percentPosition: true
+          })
+    });
+
+    $info2 = $('.info2');
+    $scrollBox = $('.scrollBox');
+    $scrollArrow
+{/* <i class="fas fa-angle-down"></i> */}
+
+
+})
